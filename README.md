8b5609479c806515ce43cc83ce37ed94ad8988cc
NilubaJashanaS <134999786+NilubaJashanaS@users.noreply.github.com>
2023-12-22 17:08:06 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# getting-started-with-the-vue-chips-component
+# Getting Started with the Vue Chips Component
+
 A quick start project that shows how to create and configure the Syncfusion Vue Chips component in a Vue app. This project also includes a code snippet to add different types of Vue Chips, add icons chip and how to customize a chip using styles, letters, and avatars and finally, how to bind a click event to the Chips component.
+ 
+Refer to the following documentation to learn about the Vue Chips component: 
+https://ej2.syncfusion.com/vue/documentation/chips/vue-3-getting-started  
+
+Check out this online example of the Vue Chips component:
+https://ej2.syncfusion.com/vue/demos/#/material/chips/default.html  
+
+## Project prerequisites
+Make sure that you have the compatible versions of [Visual Studio Code](https://code.visualstudio.com/download ) and [NodeJS](https://nodejs.org/en/download) or later version in your machine before starting to work on this project.
+
+## How to run this application
+To run this application, you need to first clone the `getting-started-with-the-vue-chips-component` repository and then open it in Visual Studio Code. Now, simply build and run your project to view the output.
---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script>
+import { ChipListComponent, ChipsDirective, ChipDirective} from '@syncfusion/ej2-vue-buttons';
+
+export default {
+  components:{
+    'ejs-chiplist': ChipListComponent,
+    'e-chips': ChipsDirective,
+    'e-chip': ChipDirective
+  },
+  data(){
+    return{
+      chipText:null
+    }
+  },
+  methods:{
+    chipClick(e){
+      if(e.event.target.classList.contains('e-chip-text')){
+        this.chipText="Sent a text"
+      }
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <ejs-chiplist selection="Multiple" @click="chipClick" enableDelete="true" >
+      <e-chips>
+        <e-chip text="Andrew" leadingIconUrl="https://ej2.syncfusion.com/vue/demos/src/chips/images/andrew.png" ></e-chip>
+        <e-chip text="Janet" leadingIconUrl="https://ej2.syncfusion.com/vue/demos/src/chips/images/janet.png" ></e-chip>
+        <e-chip text="Laura" cssClass="e-outline" ></e-chip>
+        <e-chip text="Margaret" avatarText="M" ></e-chip>
+      </e-chips>
+    </ejs-chiplist>
+    <div class="text">{{chipText}}</div>
+  </div>
+</template>
+
+<style>
+@import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-vue-buttons/styles/material.css";
+
+.text {
+  color: blue;
+  font-weight: bold;
+}
+</style>
